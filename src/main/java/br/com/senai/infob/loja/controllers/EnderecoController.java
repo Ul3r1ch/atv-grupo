55f3b1866029dc74d8365167a3ef70e6fc6152c0
Ul3r1ch <julia.ulerich@aluno.senai.br>
2025-11-28 09:24:44 -0300

    commit 2

--- src/main/java/br/com/senai/infob/loja/controllers/EnderecoController.java
+++ src/main/java/br/com/senai/infob/loja/controllers/EnderecoController.java
@@ -4,14 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.senai.infob.loja.models.Endereco;
-
 import br.com.senai.infob.loja.services.EnderecoService;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
 
 
 @RestController
